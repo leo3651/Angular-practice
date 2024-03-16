9f81d337e3c88364760ed0dcd67efe7724f763a2
Leo <leo.kowacevic@gmail.com>
2024-03-16 17:44:44 +0100

    add recipe

--- 01 project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 01 project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test',
+      'This is a test',
+      'https://www.finedininglovers.com/sites/g/files/xknfdk626/files/2021-11/02_iStock-619087354-_1_0.jpg'
+    ),
+  ];
 
+  constructor() {}
 }
