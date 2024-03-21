6644f87cb69c6b3248a1c13b3d76cb3385f2bdb5
Leo <leo.kowacevic@gmail.com>
2024-03-21 18:42:15 +0100

    practice basics

--- 00 basics practice/4 practice all/src/app/app.component.ts
+++ 00 basics practice/4 practice all/src/app/app.component.ts
@@ -7,9 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = '';
+  showDetails = false;
+  logClicks = [];
 
   onInputChange(e) {
     console.log(e);
     this.username = e.target.value;
+  }
+
+  onUpdateUsername() {
+    this.username = '';
+  }
+
+  onUpdateShowDetails() {
+    this.showDetails = !this.showDetails;
+    this.logClicks.push(
+      new Intl.DateTimeFormat('hr-HR', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        minute: 'numeric',
+        hour: 'numeric',
+        second: 'numeric',
+      }).format(new Date())
+    );
+  }
+
+  getColor(index) {
+    console.log('NEW');
+    console.log(index >= 4, index);
+    return index >= 4 ? 'lightgreen' : 'transparent';
   }
 }
--- 00 basics practice/4 practice all/src/app/app.module.ts
+++ 00 basics practice/4 practice all/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SuccessAlert } from './success-alert/success-alert.component';
 import { WarningAlert } from './warning-alert/warning-alert.component';
+import { AppServerComponent } from './app-server/app-server.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent, SuccessAlert, WarningAlert],
+  declarations: [AppComponent, SuccessAlert, WarningAlert, AppServerComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
