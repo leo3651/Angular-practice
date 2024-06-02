be325ec94603b98772b565a4409c508c503607a8
Leo <leo.kowacevic@gmail.com>
2024-06-02 13:23:31 +0200

    form controls

--- 07 handling forms/src/app/app.component.ts
+++ 07 handling forms/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('form') formElement: HTMLFormElement;
+  @ViewChild('email') emailInputElement: HTMLFormElement;
+
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(event: NgForm) {
+    console.log('Form element: ', this.formElement);
+    console.log('Form element, native element', this.formElement.nativeElement);
+    console.log('Email input element: ', this.emailInputElement);
+    console.log('Event on submit: ', event);
+    console.log('Submitted');
   }
 }
