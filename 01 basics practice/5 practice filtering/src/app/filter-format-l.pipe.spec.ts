9510e0448d5f982f6083529a0e5ca01353a25b06
Leo <leo.kowacevic@gmail.com>
2024-06-02 11:44:52 +0200

    add handling forms

--- 01 basics practice/5 practice filtering/src/app/filter-format-l.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterFormatLPipe } from './filter-format-l.pipe';
-
-describe('FilterFormatLPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterFormatLPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
