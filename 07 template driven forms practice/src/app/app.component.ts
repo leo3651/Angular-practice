668f163de93085aaae0ffc7c3a2169739a6686f8
Leo <leo.kowacevic@gmail.com>
2024-06-08 19:55:53 +0200

    finish TD forms

--- 07 template driven forms practice/src/app/app.component.ts
+++ 07 template driven forms practice/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  defaultOption = 'advanced';
 
+  submitForm(form: NgForm) {
+    console.log(form);
+  }
 }
