05b3100c6bae53565d1cb9c004a8158b66623213
Leo <leo.kowacevic@gmail.com>
2024-06-09 17:38:38 +0200

    reactive forms

--- 08 reactive forms/src/app/app.component.ts
+++ 08 reactive forms/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,54 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   form: FormGroup;
+  hobby: string = '';
+  forbiddenUsernames: string[] = ['Leon', 'Jon'];
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      username: new FormControl(null),
-      email: new FormControl(null),
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
+      }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  submitForm() {
+    console.log(this.form);
+  }
+
+  addHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.form.get('hobbies')).push(control);
+  }
+
+  get hobbiesControls() {
+    console.log((<FormArray>this.form.get('hobbies')).controls);
+    return (<FormArray>this.form.get('hobbies')).controls;
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1)
+      return { forbiddenName: true };
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@gmail.com')
+          resolve({ forbiddenMail: true });
+        resolve(null);
+      }, 2500);
     });
   }
 }
