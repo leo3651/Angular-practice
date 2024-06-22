23454b09788ae36c7bebe3083742ae1c678adfc0
Leo <leo.kowacevic@gmail.com>
2024-06-22 13:02:33 +0200

    manually created pipe

--- 09 pipes/src/app/app.component.ts
+++ 09 pipes/src/app/app.component.ts
@@ -3,40 +3,56 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  filteredStatus: string = '';
+
   servers = [
     {
       instanceType: 'medium',
       name: 'Production Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
     },
     {
       instanceType: 'large',
       name: 'User Database',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
     },
     {
       instanceType: 'small',
       name: 'Development Server',
       status: 'offline',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
     },
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
-    }
+      started: new Date(15, 1, 2017),
+    },
   ];
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+  getStatusClasses(server: {
+    instanceType: string;
+    name: string;
+    status: string;
+    started: Date;
+  }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
-      'list-group-item-danger': server.status === 'critical'
+      'list-group-item-danger': server.status === 'critical',
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'medium',
+      name: 'My Server',
+      status: 'stable',
+      started: new Date(15, 1, 2024),
+    });
   }
 }
