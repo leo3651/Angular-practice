8d186417ef39a74f5b581d417423e82d48b98725
Leo <leo.kowacevic@gmail.com>
2024-07-06 14:47:17 +0200

    add loading spinner

--- project/src/app/auth/auth.component.ts
+++ project/src/app/auth/auth.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from './auth.service';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
   isLoginMode: boolean = false;
+
+  constructor(private authService: AuthService) {}
 
   onSwitch() {
     this.isLoginMode = !this.isLoginMode;
@@ -15,6 +19,26 @@ export class AuthComponent {
   onSubmit(form: NgForm) {
     console.log(form);
     console.log(form.value);
+
+    if (!form.valid) {
+      return;
+    }
+
+    if (this.isLoginMode) {
+    } else {
+      const email = form.value.email;
+      const password = form.value.password;
+
+      this.authService.signup(email, password).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     form.reset();
   }
 }
---
+++ project/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  styleUrl: './loading-spinner.component.css',
+  template: '<div class="lds-facebook"><div></div><div></div><div></div></div>',
+})
+export class LoadingSpinnerComponent {}
