e9e6f551a90238fa1f027646e25b4bd25481a8a5
Leo <leo.kowacevic@gmail.com>
2024-08-04 11:52:22 +0200

    ngrx effects

--- 13 ngRx/src/app/store/counter.effects.ts
+++ 13 ngRx/src/app/store/counter.effects.ts
@@ -1,19 +1,37 @@
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { decrement, increment } from './counter.actions';
-import { tap } from 'rxjs';
+import { decrement, increment, init, set } from './counter.actions';
+import { of, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCount } from './counter.selector';
 
 @Injectable()
 export class CounterEffects {
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<{ counter: number }>
+  ) {}
+  loadCount = createEffect(() =>
+    this.actions$.pipe(
+      ofType(init),
+      switchMap(() => {
+        const storedCount = localStorage.getItem('count');
+        if (storedCount) {
+          return of(set({ value: +storedCount }));
+        }
+        return of(set({ value: 0 }));
+      })
+    )
+  );
 
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
         ofType(increment, decrement),
-        tap((action) => {
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
           console.log(action);
-          localStorage.setItem('count', action.value + '');
+          localStorage.setItem('count', counter.toString());
         })
       ),
     { dispatch: false }
