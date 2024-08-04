d5788f4adce3d2cdc4c787ba8d41482dbfda8bd9
Leo <leo.kowacevic@gmail.com>
2024-08-04 21:39:02 +0200

    angular animations

---
+++ 14 angular animations/src/app/app.component.ts
@@ -0,0 +1,95 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({ 'background-color': 'red', transform: 'translateX(0);' })
+      ),
+      state(
+        'highlighted',
+        style({ 'background-color': 'blue', transform: 'translateX(100px)' })
+      ),
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          'background-color': 'blue',
+          transform: 'translateX(100px) scale(1)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          'background-color': 'green',
+          transform: 'translateX(0) scale(0.5)',
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange',
+        }),
+        animate(
+          500,
+          style({
+            borderRadius: '50px',
+          })
+        ),
+        animate(500),
+      ]),
+    ]),
+    trigger('list1', [
+      state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateX(-100px)' }),
+        animate(300),
+      ]),
+      transition('* => void', [
+        animate(300, style({ opacity: 0, transform: 'translateX(100px)' })),
+      ]),
+    ]),
+  ],
+})
+export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onAnimate() {
+    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+}
